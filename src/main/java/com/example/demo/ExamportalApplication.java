9d4dc2abd91dd15e542dc182e4127ee068e81145
Adity Shaw <adityshaw99@gmail.com>
2023-10-06 00:44:37 +0530

    commit

--- src/main/java/com/example/demo/ExamportalApplication.java
+++ src/main/java/com/example/demo/ExamportalApplication.java
@@ -31,28 +31,28 @@ public class ExamportalApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		System.out.println("starting code....");
 		
-		User user = new User();
-		user.setFirstName("Adity");
-		user.setLastName("Shaw");
-		user.setUsername("shawadity01");
-		user.setPassword(this.bCryptPasswordEncoder.encode("1234"));
-		user.setEmail("abc@gmail.com");
-		user.setProfile("default.png");
-		
-		Role role1 = new Role();
-		role1.setRoleId(44L);
-		role1.setRoleName("ADMIN");
-		
-		Set<UserRole> userRoleSet = new HashSet<>();
-		UserRole userRole = new UserRole();
-		userRole.setRole(role1);
-		
-		userRole.setUser(user);
-		
-		userRoleSet.add(userRole);
-		
-		User user1 = this.userService.createUser(user, userRoleSet);
-		System.out.println(user1.getUsername());
+//		User user = new User();
+//		user.setFirstName("Adity");
+//		user.setLastName("Shaw");
+//		user.setUsername("shawadity01");
+//		user.setPassword(this.bCryptPasswordEncoder.encode("1234"));
+//		user.setEmail("abc@gmail.com");
+//		user.setProfile("default.png");
+//		
+//		Role role1 = new Role();
+//		role1.setRoleId(44L);
+//		role1.setRoleName("ADMIN");
+//		
+//		Set<UserRole> userRoleSet = new HashSet<>();
+//		UserRole userRole = new UserRole();
+//		userRole.setRole(role1);
+//		
+//		userRole.setUser(user);
+//		
+//		userRoleSet.add(userRole);
+//		
+//		User user1 = this.userService.createUser(user, userRoleSet);
+//		System.out.println(user1.getUsername());
 	}
 
 }
